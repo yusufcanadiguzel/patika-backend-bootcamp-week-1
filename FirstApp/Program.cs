0cea9ee99721849384e1c071799e1ff33171e2f2
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-15 16:57:30 +0300

    First App Completed

---
+++ FirstApp/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine("Yusufcan Adıgüzel");
+
+Console.WriteLine("Patika'nin uzman kadrosu ve tecrubeli egitmenleri sayesinde yazilim dunyasina hizli bir sekilde giris yapabilecegim icin bu bootcampteyim!");
