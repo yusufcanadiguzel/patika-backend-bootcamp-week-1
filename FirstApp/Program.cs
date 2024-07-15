61292335beea0ac152619b23dd7fae514e59c070
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-15 17:02:54 +0300

    Code Desc. Added

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,3 +1,5 @@
-﻿Console.WriteLine("Yusufcan Adıgüzel");
+﻿//Mesajı ekrana yazdırır
+Console.WriteLine("Yusufcan Adıgüzel");
 
+//Mesajı ekrana yazdırır
 Console.WriteLine("Patika'nin uzman kadrosu ve tecrubeli egitmenleri sayesinde yazilim dunyasina hizli bir sekilde giris yapabilecegim icin bu bootcampteyim!");
