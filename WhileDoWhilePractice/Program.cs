286d477c185382a2fa91c1401d78cf4a0bfe19e7
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-16 20:21:28 +0300

    While Do-While Practice Completed

---
+++ WhileDoWhilePractice/Program.cs
@@ -0,0 +1,28 @@
+﻿int counter = 0, limit;
+
+//kullanıcıdan limiti alma
+Console.Write("Lütfen bir limit değeri giriniz: ");
+limit = int.Parse(Console.ReadLine()!);
+
+//limit geçersiz bir değerse yinede bir sefer yazdırır
+if (limit <= 0)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+}
+else
+{
+    //limite kadar yazdırır
+	while (counter <= limit)
+    {
+        Console.WriteLine("Ben bir Patika'lıyım");
+        counter++;
+    }
+}
+
+//do
+//{
+//    Console.WriteLine("Ben bir Patika'lıyım");
+//    counter++;
+//} while (counter <= limit && counter >= 0);
+
+//Yukarıdada görüldüğü gibi do-while yapısı kullanıldığında en kötü koşulda dahi bir kez çalıştığında kod ciddi ölçüde azalmakta.
