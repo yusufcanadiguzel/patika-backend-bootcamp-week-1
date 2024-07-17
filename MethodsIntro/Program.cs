98269dc2d850bf6cfb0da8efd4d137f3332b1cf9
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-07-17 15:16:20 +0300

    Methods Intro Completed

---
+++ MethodsIntro/Program.cs
@@ -0,0 +1,34 @@
+﻿//şarkı sözünü yazdırır
+void Sing()
+{
+    Console.WriteLine("Forever Isn't Long Enough");
+}
+
+//rastgele birsayının ikiye bölümünden kalanı döndürür
+int GetRandomHalf()
+{
+    Random random = new Random();
+
+    var number = random.Next();
+    var result = number % 2;
+
+    return result;
+}
+
+//verilen iki sayının çarpımını döndürür
+int Multiplication(int firstNumber, int secondNumber) => firstNumber * secondNumber;
+
+//verilen ad ve soyada göre mesaj yazdırır
+void SayWelcome(string firstName, string lastName)
+{
+    Console.WriteLine($"Hoş Geldiniz {firstName} {lastName}");
+}
+
+//uygulamalar
+Sing();
+
+Console.WriteLine(GetRandomHalf());
+
+Console.WriteLine(Multiplication(firstNumber: 3, secondNumber: 3));
+
+SayWelcome(firstName: "Yusufcan", lastName: "Adıgüzel");
